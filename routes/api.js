71874a61b51cd8ed28ef71b29b60cedf0b5adb6c
h0996g <h0996g@gmail.com>
2023-04-17 16:07:55 +0100

    second Commit

---
+++ routes/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/users');
+
+// Get all users
+router.get('/users', userController.getAllUsers);
+
+// Get user by ID
+router.get('/users/:id', userController.getUserById);
+
+// Create new user
+router.post('/users', userController.createUser);
+
+// Update user
+router.put('/users/:id', userController.updateUser);
+
+// Delete user
+router.delete('/users/:id', userController.deleteUser);
+
+module.exports = router;
