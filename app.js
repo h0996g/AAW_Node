1af91e981ecb4cb1383c79f6d7d4c25f769c2e57
h0996g <h0996g@gmail.com>
2023-04-21 02:39:31 +0100

    test Cyclic

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');//installer le module mongoose (avant vous devez installer et configurer Mongodb)
@@ -6,21 +7,40 @@ const apiRouter = require('./routes/api');
 const app = express();
 
 app.use(bodyParser.json());
-
+const PORT = process.env.PORT || 3000
 // Connexion à la base de données MongoDB
+// mongoose.set('strictQuery', false);
 
-mongoose.connect('mongodb://127.0.0.1:27017/mydb', { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connexion réussie à la base de données');
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+//   .then(() => {
+//     console.log('Connexion réussie à la base de données');
 
 
 
-    app.use('/api', apiRouter);
-    var port = process.env.PORT || 3000
-    app.listen(port, () => {
-      console.log(`Server started on port ${port}`);
-    });
+//     app.use('/', apiRouter);
+//     app.listen(port, () => {
+//       console.log(`Server started on port ${port}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(`Erreur de connexion à la base de données : ${error}`);
+//   });
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(PORT);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+app.use('/', apiRouter);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
   })
-  .catch((error) => {
-    console.log(`Erreur de connexion à la base de données : ${error}`);
-  });
+})
--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const userController = require('../controllers/users');
 const router = express.Router();
